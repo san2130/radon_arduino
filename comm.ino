6744ed27a7100acce3c22f7aa0be02a54b3f4cab
Sandeepan Ghosh <sandtgbg@gmail.com>
2023-10-28 03:06:06 +0530

    Comm done

--- comm.ino
+++ comm.ino
@@ -1,11 +1,84 @@
 #include "SerialTransfer.h"
+#define USE_BASE      // Enable the base controller code
+//#undef USE_BASE     // Disable the base controller code
 
+/* Define the motor controller and encoder library you are using */
+#ifdef USE_BASE
+   /* The Pololu VNH5019 dual motor driver shield */
+   //#define POLOLU_VNH5019
+
+   /* The Pololu MC33926 dual motor driver shield */
+   //#define POLOLU_MC33926
+
+   /* The RoboGaia encoder shield */
+   //#define ROBOGAIA
+   
+   /* Encoders directly attached to Arduino board */
+   #define ARDUINO_ENC_COUNTER
+
+   /* L298 Motor driver*/
+   #define L298_MOTOR_DRIVER
+#endif
+
+//#define USE_SERVOS  // Enable use of PWM servos as defined in servos.h
+#undef USE_SERVOS     // Disable use of PWM servos
+
+/* Serial port baud rate */
+#define BAUDRATE     57600
+
+/* Maximum PWM signal */
+#define MAX_PWM        255
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+/* Include definition of serial commands */
+#include "commands.h"
+
+/* Sensor functions */
+#include "sensors.h"
+
+/* Include servo support if required */
+#ifdef USE_SERVOS
+   #include <Servo.h>
+   #include "servos.h"
+#endif
+
+#ifdef USE_BASE
+  /* Motor driver function definitions */
+  #include "motor_driver.h"
+
+  /* Encoder driver function definitions */
+  #include "encoder_driver.h"
+
+  /* PID parameters and functions */
+  #include "diff_controller.h"
+
+  /* Run the PID loop at 30 times per second */
+  #define PID_RATE           30     // Hz
+
+  /* Convert the rate into an interval */
+  const int PID_INTERVAL = 1000 / PID_RATE;
+  
+  /* Track the next time we make a PID calculation */
+  unsigned long nextPID = PID_INTERVAL;
+
+  /* Stop the robot if it hasn't received a movement command
+   in this number of milliseconds */
+  #define AUTO_STOP_INTERVAL 2000
+  long lastMotorCommand = AUTO_STOP_INTERVAL;
+#endif
 
 SerialTransfer myTransfer;
 
 char arr[12];
-char success[]="OK";
-char failure[]="ER";
+char success[20]="OK";
+char failure[20]="ER";
+char bd[20]="57600";
+char encRead[20];
 
 // A pair of varibles to help parse serial commands (thanks Fergs)
 int arg = 0;
@@ -31,17 +104,107 @@ void setup()
   myTransfer.begin(Serial);
 }
 
-int runCommand(){
+int count(int x)
+{
+  int c=0;
+  while(x)
+  {
+    c++;
+    x/=10;
+  }
+  return c;
+}
+
+void sendSuccess()
+{
     uint16_t sendSize = 0;
-    if(cmd == 'm')
-    {
-      sendSize = myTransfer.txObj(success, sendSize);
-    }
-    else
-    {
-      sendSize = myTransfer.txObj(failure, sendSize);
-    }
+    sendSize = myTransfer.txObj(success, sendSize);
     myTransfer.sendData(sendSize);
+}
+
+void sendFailure()
+{
+    uint16_t sendSize = 0;
+    sendSize = myTransfer.txObj(failure, sendSize);
+    myTransfer.sendData(sendSize);
+}
+
+void sendBaud()
+{
+    uint16_t sendSize = 0;
+    sendSize = myTransfer.txObj(bd, sendSize);
+    myTransfer.sendData(sendSize);
+}
+
+void sendEncoder()
+{
+    uint16_t sendSize = 0;
+    sendSize = myTransfer.txObj(encRead, sendSize);
+    myTransfer.sendData(sendSize);
+}
+
+int runCommand(){
+  int i = 0;
+  char *p = argv1;
+  char *str;
+  int pid_args[4];
+  arg1 = atoi(argv1);
+  arg2 = atoi(argv2);
+//  encRead[0]=cmd;
+  if(cmd=='b')
+  {
+    sendBaud();
+  }
+  else if(cmd=='e')
+  {
+    int encLeft = readEncoder(LEFT);
+    encLeft = 120;
+    int count_left = count(encLeft);
+    int encRight = readEncoder(RIGHT);
+    encRight = 1111;
+    char leftRead[10], rightRead[10];
+    sprintf(leftRead,"%d", encLeft);
+    sprintf(rightRead,"%d", encRight);
+    for(int i=0;i<count_left;i++)
+      encRead[i] = leftRead[i];
+    encRead[count_left]=' ';
+    for(int i=0;i<count(encRight);i++)
+      encRead[i+count_left+1] = rightRead[i];
+    sendEncoder();
+  }
+  else if(cmd=='r')
+  {
+    resetEncoders();
+    resetPID();
+    sendSuccess();
+  }
+  else if(cmd=='m')
+  {
+    /* Reset the auto stop timer */
+    lastMotorCommand = millis();
+    if (arg1 == 0 && arg2 == 0) {
+      setMotorSpeeds(0, 0);
+      resetPID();
+      moving = 0;
+    }
+    else moving = 1;
+    leftPID.TargetTicksPerFrame = arg1;
+    rightPID.TargetTicksPerFrame = arg2;
+    sendSuccess();
+  }
+  else if(cmd=='o')
+  {
+    /* Reset the auto stop timer */
+    lastMotorCommand = millis();
+    resetPID();
+    moving = 0; // Sneaky way to temporarily disable the PID
+    setMotorSpeeds(arg1, arg2);
+    sendSuccess();
+  }
+  else
+  {
+    sendFailure();
+  }
 }
 
 
@@ -57,14 +220,14 @@ void resetCommand() {
 }
 
 void loop()
-{
+{ 
   if(myTransfer.available())
   {
     uint16_t recSize = 0;
     uint16_t sendSize = 0;
     
     recSize = myTransfer.rxObj(arr, recSize);
-    for(int i=0;i < 12; i++)
+    for(int i=0; i < 12; i++)
     {
        char chr = arr[i];
        if (chr == 13) {
