8df542f8b6d5353e56a36c0abc29c4807f8f735b
Sandeepan Ghosh <sandtgbg@gmail.com>
2023-11-03 23:04:23 +0530

    Final

--- encoder_driver.h
+++ encoder_driver.h
@@ -6,8 +6,8 @@
 #ifdef ARDUINO_ENC_COUNTER
   //below can be changed, but should be PORTD pins; 
   //otherwise additional changes in the code are required
-  #define LEFT_ENC_PIN_A 0  //pin 2
-  #define LEFT_ENC_PIN_B 1  //pin 3
+  #define LEFT_ENC_PIN_A A2  //pin 2
+  #define LEFT_ENC_PIN_B A1  //pin 3
   
   //below can be changed, but should be PORTC pins
   #define RIGHT_ENC_PIN_A 2  //pin A4
--- motor_driver.h
+++ motor_driver.h
@@ -4,14 +4,14 @@
 
 #ifdef L298_MOTOR_DRIVER
   #define RIGHT_MOTOR_BACKWARD 6
-  #define LEFT_MOTOR_BACKWARD  7
+  #define LEFT_MOTOR_BACKWARD  12
   #define BACK_MOTOR_BACKWARD 8
-  #define RIGHT_MOTOR_FORWARD  9
-  #define LEFT_MOTOR_FORWARD   10
+  #define RIGHT_MOTOR_FORWARD  7
+  #define LEFT_MOTOR_FORWARD   13
   #define BACK_MOTOR_FORWARD  A0
-  #define RIGHT_MOTOR_PWM 11
-  #define LEFT_MOTOR_PWM 12
-  #define BACK_MOTOR_PWM 13
+  #define RIGHT_MOTOR_PWM 9
+  #define LEFT_MOTOR_PWM 10
+  #define BACK_MOTOR_PWM 11
 #endif
 
 void initMotorController();
